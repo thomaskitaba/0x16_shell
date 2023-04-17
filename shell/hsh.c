c0f3f71d77c8b7a68c8d2bedaaf5e2a833a1ddf3
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-17 22:49:25 +0300

    --no-edit

--- shell/hsh.c
+++ shell/hsh.c
@@ -2,27 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
-/**
- * tokenize_string - change string to 2D array
- * @str: string to be tokenized
- * Return: 1 on success, -1 on failur
- */
-char **tokenize_string(char *str, char **token, int *len)
-{
-  /*char str[] = "thomas kitaba feyissa";*/
-  char *delimeter = " ";
-  int i, w_count;
-  w_count = 0;
-  /*malloc for 2d array **token  row*/
 
-  w_count = _strtok(str, delimeter, token);
-  _print_2d(token, w_count + 1);
-  *len = (w_count + 1);
-  printf("we are here\n");
-  /*free memory */
-  _free_2D(token, w_count);
-  return (token);
-}
 int hsh(int argc, char **argv)
 {
   int i, result, w_len;
