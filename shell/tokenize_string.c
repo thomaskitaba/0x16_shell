77da9adc5cdf49ed3c7993c4558fc3ddf032f302
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-17 14:28:10 +0300

    update

--- shell/tokenize_string.c
+++ shell/tokenize_string.c
@@ -1,18 +1,5 @@
 #include "main.h"
-/**
- * _print_2d - print 2D array
- * @av: argument vector
- * @argc: argument count
- * Return: void
- */
-void _print_2d(char **av, int argc)
-{
-  int i; 
-for (i = 0; i < argc - 1; i++)
-{
-  printf("%s\n", av[i]);
-}
-}
+
 /**
  * tokenize_string - change string to 2D array
  * @str: string to be tokenized
