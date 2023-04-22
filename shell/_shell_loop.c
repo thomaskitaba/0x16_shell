235a2eb21119bfce9e1aad28f01bf8b23e3a252a
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-22 22:52:41 +0300

    update

--- shell/_shell_loop.c
+++ shell/_shell_loop.c
@@ -11,7 +11,7 @@
   do{
   av_token = create_2D_buffer(av_token); 
   buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-  if (mode != 0)
+  if (argv[0] != "|")
   putchar('$');
   read = getline(&buffer, &len, stdin);
   /*TODO: we have to remove the new line from buffer*/
