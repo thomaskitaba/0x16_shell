56a86b3a3cce7609ab125c63dae2dd3cdd5fe6ab
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-22 23:43:47 +0300

    betty get built in

--- shell/_get_builtin_cmd.c
+++ shell/_get_builtin_cmd.c
@@ -2,11 +2,10 @@
 /**
  * get_builtin_cmd - get command
  * @s: string command
- * Return: integer 
+ * Return: integer
  */
 int (*get_builtin_cmd(char *s))(char **)
 {
-
 built_in b_in[] = {
 {"Exit", _exit_shell},
 {"printenv", _printenv},
