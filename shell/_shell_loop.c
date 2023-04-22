0b5fcee4fbd89fc388c368f1c657975a8b84f87d
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-22 13:34:58 +0300

    added 2_2d_string function

--- shell/_shell_loop.c
+++ shell/_shell_loop.c
@@ -21,7 +21,7 @@
   if (strcmp(buffer, "exit") == 0) {
     b_string = create_2D_buffer(b_string);
     token(b_string, buffer);
-    result = _exit_shell(token);
+    result = _exit_shell(b_string);
   }
   if (read == -1) {
   	free(buffer);
