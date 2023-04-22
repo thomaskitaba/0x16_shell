dac9b62dc02884ec61fb6d5b0ac38f2d75d672e4
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-22 23:38:00 +0300

    betty execve

--- shell/_execve.c
+++ shell/_execve.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _execve - execve 
+ * _execve - execve
  * @av: 2D of CL arguments
  * @env: environment variable
  * Return: succes 1, failur -1.
@@ -16,23 +16,25 @@ pid_t child_pid;
 if (stat(av[0], &st) == 0)
 child_pid = fork();
 else {
-  perror(av[0]);
-  return (-1);
+perror(av[0]);
+return (-1);
 }
 if (child_pid == 0) {
-if (execve(av[0], av, env) == -1){
-    perror("Error insid execve:");
-    return (-1);
+if (execve(av[0], av, env) == -1)
+{
+perror("Error insid execve:");
+return (-1);
 }
 }
-else if (child_pid > 0){
+else if (child_pid > 0)
+{
 wait(&status);
 return (1);
 }
 else{
-
 return (-1);
 /*TODO: update exit struct*/
-return (-1); }
+return (-1);
+}
 return (-1);
 }
