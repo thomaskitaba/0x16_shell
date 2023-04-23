6d83a85cf6b79b92e0378ecb7b58603603e1c539
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-23 12:07:00 +0300

    betty hsh.c

--- shell/hsh.c
+++ shell/hsh.c
@@ -3,37 +3,23 @@
  * hsh - the shell part
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 or 1 
+ * @mode: open mode
+ * Return: 0 or 1
  */
 int hsh(int argc, char **argv, int mode)
 {
-  int i, hsh_val; ssize_t read;
-  size_t len; char **av;
+int i, hsh_val; ssize_t read;
+size_t len; char **av;
 
-if (argc == 1 && (strcmp(argv[0], "./hsh") == 0)) {
-  printf("interactive mode\n");
-  hsh_val = shell_loop_hsh(argc, argv, mode);
-  /*if (hsh_val == -1)
-    perror("Error insid hsh: ");*/
-  if (hsh_val == 0)
-    return (0);
+if (argc == 1 && (strcmp(argv[0], "./hsh") == 0))
+{
+printf("interactive mode\n");
+hsh_val = shell_loop_hsh(argc, argv, mode);
+if (hsh_val == 0)
+return (0);
 }
 else{
-  exit(1);
+exit(1);
 }
-/*else {
-  av = (char**)malloc(sizeof(char *) * (argc + 1));
-  for (i = 0; i < argc + 1; i++)   {
-    av[i] = (char *)malloc(sizeof(char) * _strlen(av[i] + 1));
-    strcpy(av[i], argv[i]);
-    if (i + 1 == argc) {
-      av[i + 1] = NULL;
-      break;
-    }
-  }
-  _execve(av, NULL);
-
-}
-_free_2D(av, argc + 1);*/
 return (0);
 }
