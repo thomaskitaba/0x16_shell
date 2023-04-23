42825833e4fd5924878d2a4b7278dbc26f3ac267
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-23 11:33:52 +0300

    betty main.c

--- shell/main.c
+++ shell/main.c
@@ -1,22 +1,19 @@
 #include "main.h"
 /**
  * main - entry point of the program
- * 
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env)
 {
-  int a, mode; /*to accept returned value from hsh*/
-  /*initialize some variables here*/
-  /*step 1: send string and recive 2d array*/
-  /*step 2: send the 2d array to tokenzier
-  */
+  int a;
+  mode; /*to accept returned value from hsh*/
+  
   if (argc <= 1)
   mode = 1; /*interactive*/
   else 
   mode = 0; /*command line mode*/
-
   hsh(argc, argv, mode); 
-
   return (0);
 }
