cbc4661033411bbff19478d88f2395b8e3a327e9
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-23 14:15:49 +0300

    reduced add_path to path_correction and add_path.c

--- shell/add_path.c
+++ shell/add_path.c
@@ -4,28 +4,28 @@
  * @path_buffer: buffer holding string
  * Return: append path return1,  path not appended, -1
  */
-char *add_path(char *path_arg, char *path_buffer)
+char *add_path(char *path_arg, char *path_buf)
 {
-char *default_path; /*TODO: to be deleted*/
-int path_len, res, default_len, buffer_len, correction;
+char *def_path; /*TODO: to be deleted*/
+int path_len, res, def_len, buffer_len, correction;
 int i, k; /*TODO: to be deleted*/
 path_len = buffer_len = k = 0;
-default_path = "/bin/"; /*TODO: to be deleted*/
-default_len = _strlen(default_path);
+def_path = "/bin/"; /*TODO: to be deleted*/
+def_len = _strlen(def_path);
 path_len = _strlen(path_arg);
 correction = res = 0;
 /*check if /bin/ exitst in path_arg*/
 for (i = 0; i < default_len; i++)
 {
-if (default_path[i] != path_arg[i])
+if (def_path[i] != path_arg[i])
 {
 correction = 1;
 break;
 }
 }
-path_buffer = path_helper(path_arg, path_buffer, correction, default_len, path_len);
-if (path_buffer)
-return (path_buffer);
+path_buf = path_helper(path_arg, path_buf, correction, def_len, path_len);
+if (path_buf)
+return (path_buf);
 else
 return (NULL);
 }
