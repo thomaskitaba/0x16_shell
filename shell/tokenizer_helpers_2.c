83017b113d4cf029ceecee4ff8e7ad899835df1f
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-23 12:36:35 +0300

    betty tokenize helper 2.c

--- shell/tokenizer_helpers_2.c
+++ shell/tokenizer_helpers_2.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/** add_path: add path to cmd if not provided
+/**
  * @path_arg: path argument at index 0
  * @path_buffer: buffer holding string
  * Return: append path return1,  path not appended, -1
  */
-char* add_path(char *path_arg, char *path_buffer)
+char *add_path(char *path_arg, char *path_buffer)
 {
 char *default_path;
 int i, k, path_len, default_len, buffer_len, new_len, need_correction;
@@ -14,7 +14,8 @@ default_path = "/bin/"; default_len = _strlen(default_path);
 path_len = _strlen(path_arg);
 need_correction = 0;
 /*check if /bin/ exitst in path_arg*/
-for (i = 0; i < default_len; i++) {
+for (i = 0; i < default_len; i++)
+{
 if (default_path[i] != path_arg[i])
 {
 need_correction = 1;
@@ -35,7 +36,8 @@ return (path_buffer);
 }
 if (need_correction == 0)
 {
-if (path_len <= 5) {
+if (path_len <= 5)
+{
 printf("it is a directory");
 return (NULL);
 }
