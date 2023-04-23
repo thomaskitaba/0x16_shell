2cd66858c9d518921c1f17ad85f0ca7c20ccdbba
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-23 12:14:32 +0300

    betty strings_1.c

--- shell/strings_1.c
+++ shell/strings_1.c
@@ -2,31 +2,31 @@
 /**
  * _get_word_count - length of string
  * @str: string
- * Return: int 
+ * @delim: delimeter
+ * Return: int
  */
 int _get_word_count(char *str, char *delim)
 {
-  int word_count = 0;
-  char *token;
-  token = strtok(str, delim);
-  while (token)
-  {
-    word_count++;
-    token = strtok(NULL, delim);
-  }
-  return word_count;
+int word_count = 0;
+char *token;
+token = strtok(str, delim);
+while (token)
+{
+word_count++;
+token = strtok(NULL, delim);
+}
+return word_count;
 }
 /**
  * _strlen - length of string
  * @str: string
- * Return: int 
+ * Return: int
  */
 int _strlen(char *str)
 {
-  int i;
-  i = 0;
-  while(str[i] != '\0') {
-    i++;   }
+int i;
+i = 0;
+while(str[i] != '\0')
+i++;
 return (i);
 }
-
