683b63f4f1a768c006c17f64cc6a5424cce576bf
thomaskitaba <thomas.kitaba@gmail.com>
2023-04-23 11:43:47 +0300

    betty memory.c

--- shell/memory.c
+++ shell/memory.c
@@ -1,37 +1,33 @@
 #include "main.h"
 /**
  * _free_2D - free 2D array
- * @2d: 2d array
+ * @token: 2d array
+ * @rows: row
  * Return: void
  */
 void _free_2D(char **token, int rows)
 {
 int i;
-for (int i = 0; i < rows; i++) {
-    free(token[i]);
-}
+for (int i = 0; i < rows; i++)
+free(token[i]);
 free(token);
 }
-/**
- * create_buffer - create buffer
- * Return: buffer
- */
 char *create_buffer(void)
 {
 char *buf;
 buf = malloc(sizeof(char) * BUFFER_SIZE);
 if (!buf)
-        return (NULL);
+return (NULL);
 return (buf);
 }
 char **create_2D_buffer(char **av)
 {
-  int i;
-  av = (char **)malloc(sizeof(char *) * MAX_WORDS);
-  if (!av) {
-    return (NULL);
-  }
-for (i = 0; i < MAX_WORDS; i++) {
+int i;
+av = (char **)malloc(sizeof(char *) * MAX_WORDS);
+if (!av) 
+return (NULL);
+for (i = 0; i < MAX_WORDS; i++)
+{
 av[i] = (char *)malloc(sizeof(char) * MAX_WORD_LENGTH);
 if (!av[i])
 return (NULL);
